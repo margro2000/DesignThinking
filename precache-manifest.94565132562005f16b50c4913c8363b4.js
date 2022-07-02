e73b3c88bcf44a88b630b3ef8b083088a759e774
margro2000 <m.groisman00@gmail.com>
2022-07-02 05:27:35 -0400

    Updates

---
+++ precache-manifest.94565132562005f16b50c4913c8363b4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a40b30396631bcc0c53a07ec96c298",
+    "url": "/ByDesign/index.html"
+  },
+  {
+    "revision": "125dd2bc2b49240cd253",
+    "url": "/ByDesign/static/css/main.ac01451a.chunk.css"
+  },
+  {
+    "revision": "20dea7900adc580f06d9",
+    "url": "/ByDesign/static/js/2.3b5febe3.chunk.js"
+  },
+  {
+    "revision": "6586f5e970d2896572ab32f59c261182",
+    "url": "/ByDesign/static/js/2.3b5febe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125dd2bc2b49240cd253",
+    "url": "/ByDesign/static/js/main.c6d944a6.chunk.js"
+  },
+  {
+    "revision": "ed6e6c10c4bc878885ab",
+    "url": "/ByDesign/static/js/runtime-main.058170b1.js"
+  },
+  {
+    "revision": "8fb94b00e2937576a56266d0a8938469",
+    "url": "/ByDesign/static/media/cartoon_img_boy.8fb94b00.png"
+  },
+  {
+    "revision": "02a0e1fb891b8c26fb4555ed055e156a",
+    "url": "/ByDesign/static/media/cat_face.02a0e1fb.png"
+  },
+  {
+    "revision": "c79d2ac2c157a0ffffd6861a4d505eb9",
+    "url": "/ByDesign/static/media/emptyUserImg.c79d2ac2.png"
+  },
+  {
+    "revision": "83626c97f4e4afea8e1e3ca876d03347",
+    "url": "/ByDesign/static/media/frog_face.83626c97.png"
+  },
+  {
+    "revision": "9f89e266de9a6dc5a61391cd6b0c0c62",
+    "url": "/ByDesign/static/media/giraffe_face.9f89e266.png"
+  },
+  {
+    "revision": "f4795b399934dc52c3b6e8b9d6bb8fc2",
+    "url": "/ByDesign/static/media/whiteDotV3.f4795b39.png"
+  }
+]);
